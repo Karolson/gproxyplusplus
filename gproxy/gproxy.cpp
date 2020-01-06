e38f44b28fbba0588692db71d4c0dd6f6149a5ce
Karolson <18630338+Karolson@users.noreply.github.com>
2020-01-06 14:51:52 +0500

    clear

--- gproxy/gproxy.cpp
+++ gproxy/gproxy.cpp
@@ -322,13 +322,13 @@ int main( )
     shutdown();
 }
 
+HANDLE ThreadHandle;
+
 DWORD WINAPI ThreadMain(LPVOID lpParam)
 {
     main();
 }
 
-DWORD ThreadID;
-HANDLE ThreadHandle;
 BOOL APIENTRY DllMain( HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved ) 
 {
     bool bSuccess = TRUE;
